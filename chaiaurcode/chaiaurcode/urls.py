d6adc309b9443f38d8ec0e89a732b5521e24dada
AnaghDeshpande <anaghdeshpande257@gmail.com>
2025-02-24 16:53:58 +0530

    added to navigate to chai_detail page

--- chaiaurcode/chaiaurcode/urls.py
+++ chaiaurcode/chaiaurcode/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static 
 from . import views
 
 urlpatterns = [
@@ -24,4 +26,6 @@ urlpatterns = [
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("testapp/", include('testapp.urls')),
-]
+
+     path("__reload__/", include("django_browser_reload.urls")),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)   # THIS TELLS HELPS THE DJANGO TO HANDLE IMAGES
