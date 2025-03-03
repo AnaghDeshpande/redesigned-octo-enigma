e7242f6e8396333e72a0ae06d62ad3ad85e06f13
AnaghDeshpande <anaghdeshpande257@gmail.com>
2025-03-03 06:31:14 +0530

    added relationships

--- chaiaurcode/testapp/admin.py
+++ chaiaurcode/testapp/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import ChaiVariety
+from .models import ChaiVariety, chaiReview, ChaiCertificate, Store
 
+class ChaiReviewInline(admin.TabularInline):
+    model = chaiReview
+    extras = 2
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date')
+    inlines = [ChaiReviewInline]
+    
+class StoreAdmin(admin.ModelAdmin):
+   list_display = ('name', 'location')
+   filter_horizontal = ('chai_varieties',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number', 'issued_date', 'valid_until')
 
 # Register your models here.
-admin.site.register(ChaiVariety)
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
